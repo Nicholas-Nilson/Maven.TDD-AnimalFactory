bfc609d39c2420567d8ac3856cf97fceb1224fee
Nick Nilson <nilson.nicholas@gmail.com>
2022-07-05 09:01:06 -0400

    save before assessment, working on DogHouse tests

--- src/test/java/rocks/zipcodewilmington/DogHouseTest.java
+++ src/test/java/rocks/zipcodewilmington/DogHouseTest.java
@@ -1,8 +1,11 @@
 package rocks.zipcodewilmington;
 
+import org.junit.Assert;
 import org.junit.Test;
+import rocks.zipcodewilmington.animals.Cat;
 import rocks.zipcodewilmington.animals.Dog;
 import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
+import rocks.zipcodewilmington.animals.animal_storage.CatHouse;
 import rocks.zipcodewilmington.animals.animal_storage.DogHouse;
 
 import java.util.Date;
@@ -12,23 +15,44 @@ import java.util.Date;
  */
 public class DogHouseTest {
     // TODO - Create tests for `void add(Dog dog)`
+    @Test
+    public void addDogTest() {
+        //Given
+        Dog testDog = new Dog("Marlow", null, 4);
+        DogHouse.add(testDog);
+        //When
+        Dog actualDog = DogHouse.getDogById(4);
+        //Then
+        Assert.assertEquals(testDog, actualDog);
+    }
     // TODO - Create tests for `void remove(Integer id)`
+
+    @Test
+    public void removeDogTest() {
+        //Given
+        Dog testDog = new Dog("Alfalfa", null, 13);
+        DogHouse.add(testDog);
+        DogHouse.remove(13);
+        //When
+        Dog actualDog = DogHouse.getDogById(13);
+        //Then
+        Assert.assertNull(actualDog);
+    }
+
+    @Test
+    public void removeDogByNameTest() {
+        //Given
+        Dog testDog = new Dog("Alfalfa", null, 13);
+        DogHouse.add(testDog);
+        DogHouse.remove(testDog);
+        //When
+        Dog actualDog = DogHouse.getDogById(13);
+        //Then
+        Assert.assertNull(actualDog);
+    }
     // TODO - Create tests for `void remove(Dog dog)`
     // TODO - Create tests for `Dog getDogById(Integer id)`
     // TODO - Create tests for `Integer getNumberOfDogs()`
 
-    @Test
-    public void testGetNumberOfDogs() {
-        // Given (some
-        String name = "Milo";
-        Date birthDate = new Date();
-        Dog animal = AnimalFactory.createDog(name, birthDate);
-        DogHouse.clear();
 
-        // When
-        DogHouse.add(animal);
-
-        // Then
-        DogHouse.getNumberOfDogs();
-    }
 }
